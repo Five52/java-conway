903b245dfa20fafea1aa6bfca97cc4de1b71679d
Five52 <five52@laposte.net>
2016-12-01 10:46:05 +0100

    Add class GameOfLife

---
+++ src/model/GameOfLife.java
@@ -0,0 +1,12 @@
+package model;
+
+public class GameOfLife {
+
+    public Fish[][] cases;
+
+    public GameOfLife(int size, int nbSharks, int nbPilchards) {
+        // cases = new Fish[size][size];
+    }
+
+
+}
