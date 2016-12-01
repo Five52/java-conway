e22af9dfb30ecdabebfa9df874938cb09bbc62e4
Five52 <five52@laposte.net>
2016-12-01 17:40:01 +0100

    Init GameOfLife

--- src/model/GameOfLife.java
+++ src/model/GameOfLife.java
@@ -2,11 +2,75 @@ package model;
 
 public class GameOfLife {
 
-    public Fish[][] cases;
+    public Element[][] elements;
+    public int width;
+    public int height;
 
-    public GameOfLife(int size, int nbSharks, int nbPilchards) {
-        // cases = new Fish[size][size];
+    public GameOfLife(int width, int height, int nbSharks, int nbPilchards) {
+        this.width = width;
+        this.height = height;
+        this.elements = new Element[width][height];
+
+        this.initGame(nbSharks, nbPilchards);
+        this.randomize();
+    }
+    
+    public int getWidth() {
+        return this.width;
     }
 
+    public int getHeight() {
+        return this.height;
+    }
 
+    public Element getElement(int x, int y) {
+        return this.elements[x][y];
+    }
+
+    public void kill(Fish fish) {
+        this.
+    }
+
+    protected void initGame(int nbSharks, int nbPilchards) {
+        int x;
+        int y;
+        for (int i = 0; i < nbSharks; i++) {
+            x = i % this.width;
+            y = i / this.width;
+            this.elements[x][y] = new Shark(this, x, y);
+        }
+        for (int i = nbSharks; i < nbSharks + nbPilchards; i++) {
+            x = i % this.width;
+            y = i / this.width;
+            this.elements[x][y] = new Pilchard(this, x, y);
+        }
+        for (int i = nbSharks + nbPilchards; i < this.width * this.height; i++) {
+            x = i % this.width;
+            y = i / this.width;
+            this.elements[x][y] = new Sea(this, x, y);
+        }
+    }
+
+    protected void randomize() {
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.width; j++) {
+                
+                int x = (int) (Math.random() * this.width);
+                int y = (int) (Math.random() * this.height);
+
+                // Swap cells in the game
+                Element temp = this.elements[i][j];
+                this.elements[i][j] = this.elements[x][y];
+                this.elements[x][y] = temp;
+
+                // Swap cells coords
+                int tempX = this.elements[i][j].getX();
+                int tempY = this.elements[i][j].getY();
+                this.elements[i][j].setX() = this.elements[x][y].getX();
+                this.elements[i][j].setY() = this.elements[x][y].getY();
+                this.elements[x][y].setX() = tempX;
+                this.elements[x][y].setY() = tempY;
+            }
+        }
+    }
 }
---
+++ src/model/Sea.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Sea extends Element {
+    
+    public Sea(int x, int y) {
+        super(x, y);
+    }
+}
