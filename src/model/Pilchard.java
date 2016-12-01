9915e02effb87ebd16e8779a8cbb2128b81c7552
Thomas Fortin <contact@thomasfortin.fr>
2016-12-01 19:08:31 +0100

    Add Sea override functions and Pilchard constructor

--- src/model/Pilchard.java
+++ src/model/Pilchard.java
@@ -6,6 +6,13 @@ public class Pilchard extends Fish {
     protected static final int REPRODUCTION_INTERVAL = 4;
     protected static final String DISPLAY = ".";
 
+    /**
+     * Builds a new pilchard.
+     */
+    public Pilchard(int x, int y) {
+        super(x, y);
+    }
+
     @Override
     public int getMaxAge() {
         return MAX_AGE;
--- src/model/Sea.java
+++ src/model/Sea.java
@@ -2,7 +2,27 @@ package model;
 
 public class Sea extends Element {
     
+    protected static final String DISPLAY = " ";
+
+    /**
+     * Builds a new Sea element.
+     */
     public Sea(int x, int y) {
         super(x, y);
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
+    }
+
+    @Override
+    public int getReproductionInterval() {
+        return REPRODUCTION_INTERVAL;
+    }
+
+    @Override
+    public String getDisplay() {
+        return display;
     }
 }
